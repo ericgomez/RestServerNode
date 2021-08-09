7e158731117df1eb6db96b8065a5c9bed584363c
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-08-09 10:53:16 -0500

    Creando modelo de Usuario

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const { Schema, model, modelNames } = require('mongoose');
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    required: [true, 'The name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'The email is required'],
+  },
+  password: {
+    type: String,
+    required: [true, 'The password is required'],
+  },
+  img: {
+    type: String,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['ADMIN_ROLE', 'USER_ROLE'], //Defaults Options
+  },
+  state: {
+    type: Boolean,
+    default: true,
+  },
+  google: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+// Export function using model
+module.exports = model('User', UserSchema);
