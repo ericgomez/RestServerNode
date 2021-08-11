d8bdf466389135b45cfb8646f110fa081a45f7b8
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-08-11 15:31:20 -0500

    Optimizar importaciones en Node

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { validateFields } = require('../middlewares/validate-fields');
-const { validateJWT } = require('../middlewares/validate-jwt');
-const { isAdminRole, hasRole } = require('../middlewares/validate-roles');
+const { validateFields, validateJWT, isAdminRole, hasRole } = require('../middlewares');
 
 const { isRoleValid, existEmail, existUserById } = require('../helpers/db-validators');
 const { usersGet, usersPost, usersPut, usersDelete } = require('../controllers/users');
