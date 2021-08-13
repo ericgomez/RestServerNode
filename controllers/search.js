683b24704228abb88389c07a3c1b303cc8c37828
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-08-13 12:24:19 -0500

    Crer busquedas para las colecciones de Categorias y Productos

--- controllers/search.js
+++ controllers/search.js
@@ -29,6 +29,48 @@ const userSearch = async (end = '', res = response) => {
   });
 };
 
+const categoriesSearch = async (end = '', res = response) => {
+  const isMongoID = ObjectId.isValid(end); // True
+
+  if (isMongoID) {
+    const category = await Category.findById(end);
+    res.json({
+      results: category ? [category] : [], // Validate
+    });
+  }
+
+  // TODO: expression insensitive a uppercase and lowercase of the
+  const regex = new RegExp(end, 'i');
+
+  // TODO: search by name
+  const categories = await Category.find({ name: regex, status: true }); // search by name
+
+  res.json({
+    results: categories,
+  });
+};
+
+const productsSearch = async (end = '', res = response) => {
+  const isMongoID = ObjectId.isValid(end); // True
+
+  if (isMongoID) {
+    const product = await Product.findById(end).populate('category', 'name');
+    res.json({
+      results: product ? [product] : [], // Validate
+    });
+  }
+
+  // TODO: expression insensitive a uppercase and lowercase of the
+  const regex = new RegExp(end, 'i');
+
+  // TODO: search by name
+  const products = await Product.find({ name: regex, status: true }).populate('category', 'name'); // search by name
+
+  res.json({
+    results: products,
+  });
+};
+
 const search = (req = request, res = response) => {
   const { collection, end } = req.params;
 
@@ -43,8 +85,10 @@ const search = (req = request, res = response) => {
       userSearch(end, res);
       break;
     case 'categories':
+      categoriesSearch(end, res);
       break;
     case 'products':
+      productsSearch(end, res);
       break;
 
     default:
