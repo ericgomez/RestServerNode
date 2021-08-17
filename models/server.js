d76578b4b4ef674c59e6eab0982f54067210d6ac
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-08-17 16:22:06 -0500

    Configurar Socket.io

--- models/server.js
+++ models/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
+const { createServer } = require('http');
 
 const { dbConnection } = require('../database/config');
+const { socketController } = require('../sockets/controller');
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
+    this.server = createServer(this.app);
+    this.io = require('socket.io')(this.server);
 
     this.paths = {
       auth: '/api/auth',
@@ -26,6 +30,9 @@ class Server {
 
     // Routes of application
     this.routes();
+
+    // Sockets
+    this.sockets();
   }
 
   async connectDB() {
@@ -62,8 +69,12 @@ class Server {
     this.app.use(this.paths.uploads, require('../routes/uploads'));
   }
 
+  sockets() {
+    this.io.on('connection', socketController);
+  }
+
   listen() {
-    this.app.listen(this.port, () => {
+    this.server.listen(this.port, () => {
       console.log('Server listing in port', this.port);
     });
   }
